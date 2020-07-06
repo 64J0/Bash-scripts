f1cd5f6d1f669137b64c545215cf042a9976401d
Vinicius Gajo <vinigaio97@gmail.com>
2020-07-06 17:05:56 -0300

    Update: teste-de-ping.sh

--- src/teste-de-ping.sh
+++ src/teste-de-ping.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-for counter in $(seq 1 255)
+# trap ctrl-c and call ctrl_c()
+trap ctrl_c INT
+
+function ctrl_c() {
+	echo "** Trapped CTRL-c and finishing the process"
+	exit 0
+}
+
+for counter in $(seq 1 254)
 do
 	ping -c 1 "10.0.0.$counter"
 done
